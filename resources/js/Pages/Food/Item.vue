cd5b6b1050645cc6a0d42f2e0a9a6c2f99511fe8
Arnaud-Dubois <arnaud.dubois.work@gmail.com>
2023-06-08 13:27:42 +0200

    add food item page

---
+++ resources/js/Pages/Food/Item.vue
@@ -0,0 +1,44 @@
+<script setup lang="js">
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import FoodItemStatsRow from './Partials/FoodItemStatsRow.vue';
+import FoodItemHeader from './Partials/FoodItemHeader.vue';
+import { Head } from '@inertiajs/vue3';
+
+defineProps(['food'])
+
+/* TODO : Should be better to have all the statistics in a list */
+
+</script>
+ 
+<template>
+    <Head title="Food list item" />
+ 
+    <AuthenticatedLayout>
+        <div class="max-w-xl mx-auto p-4 sm:p-6 lg:p-8">
+
+            <header>
+                <FoodItemHeader
+                    :title="food.name"
+                />
+            </header>
+            <div>
+                <h4>Statistiques</h4>
+                <ul>
+                    <FoodItemStatsRow title="calories" :value="food.calories" />
+                    <FoodItemStatsRow title="proteines" :value="food.proteines" />
+                    <FoodItemStatsRow title="glucides" :value="food.glucides" />
+                    <FoodItemStatsRow title="lipides" :value="food.lipides" />
+                </ul>
+                <h4>Informations</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt aliquid sunt odit architecto soluta nam id, ullam voluptatum provident possimus a nihil, repellat facilis? Deserunt excepturi temporibus mollitia doloribus omnis?</p>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
+
+<style>
+h4 {
+    color: var(--main-color);
+    margin: .5rem 0;
+}
+</style>
---
+++ resources/js/Pages/Food/Partials/FoodItemStatsRow.vue
@@ -0,0 +1,31 @@
+<script setup lang="js">
+
+defineProps({
+    title: {
+        type: String,
+        required: true
+    },
+    value: {
+        type: Number,
+        required: true
+    }
+})
+
+</script>
+
+<template>
+    <li class="food-item-stats-row">
+        <div>icon</div>
+        <div>{{ title }}</div>
+        <div>{{ value }} kcal*</div>
+    </li>
+</template>
+
+<style scoped>
+.food-item-stats-row {
+    display: flex;
+    justify-content: space-around;
+    background-color: #FAFAFA;
+    padding: 0.25rem;
+}
+</style>
